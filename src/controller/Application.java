fcb27173b2262c9240c7914204a8e9d9598b65d2
gudonghee2000 <gudonghee2000@gmail.com>
2021-08-17 14:33:54 +0900

    feat : 난수 생성과 메인함수를 생성했습니다.

--- src/controller/Application.java
+++ src/controller/Application.java
@@ -1,18 +1,15 @@
 package controller;
 
+import model.Baseball;
 import model.InputBalls;
 import model.SystemBalls;
+import model.hint.strike;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-
-        SystemBalls systemBalls = new SystemBalls();
-        systemBalls.createSystemBalls();
-
-        InputBalls inputBalls = new InputBalls();
-        inputBalls.createInputBalls(scanner.nextLine().split(""));
     }
 }
